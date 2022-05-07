f6d8e657cb8f5f2f1798801916bc00112c720d38
Robert Pliszka <robplisz@gmail.com>
2022-05-07 10:29:50 -0400

    Wrote failing test for Intern

---
+++ __tests__/Intern.test.js
@@ -0,0 +1,22 @@
+const Intern = require('../lib/Intern.js');
+
+// Test for intern school
+test('intern school', () => {
+    const intern = new Intern('School');
+
+    expect(intern.school).toEqual(expect.any(String));
+});
+
+// Test for getSchool()
+test('get school name', ()=> {
+    const intern = new Intern('School');
+
+    expect(intern.getSchool().toEqual(expect.any(String)));
+});
+
+// Test getRole()
+test('get intern role', () => {
+    const intern = new Intern('School');
+
+    expect(intern.getRole().toBe('Intern'));
+});
