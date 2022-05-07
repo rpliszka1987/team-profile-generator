e1a45cee47c8a45aafb67e772b1bf2f95d44c2da
Robert Pliszka <robplisz@gmail.com>
2022-05-07 10:34:02 -0400

    Upadetd Engineer and Intern new object arguments

--- __tests__/Engineer.test.js
+++ __tests__/Engineer.test.js
@@ -2,21 +2,21 @@ const Engineer = require('../lib/Engineer.js');
 
 // Tests for Engineer github username
 test('check for github username', ()=> {
-    const engineer = new Engineer('rpliszka1987');
+    const engineer = new Engineer('Rob', 23, 'rob@test.com','rpliszka1987');
 
     expect(engineer.github).toEqual(expect.any(String));
 });
 
 // Test for getGitHub()
 test('get github account link', () => {
-    const engineer = new Engineer('rpliszka1987');
+    const engineer = new Engineer('Rob', 23, 'rob@test.com','rpliszka1987');
 
     expect(engineer.getGithub().toEqual(expect.any(String)));
 });
 
 // Test for Engineer role
 test('get Engineer role', () => {
-    const engineer = new Engineer('rpliszka1987');
+    const engineer = new Engineer('Rob', 23, 'rob@test.com','rpliszka1987');
 
     expect(engineer.getRole().toBe('Engineer'));
 });
--- __tests__/Intern.test.js
+++ __tests__/Intern.test.js
@@ -2,21 +2,21 @@ const Intern = require('../lib/Intern.js');
 
 // Test for intern school
 test('intern school', () => {
-    const intern = new Intern('School');
+    const intern = new Intern('Rob', 23, 'rob@test.com','School');
 
     expect(intern.school).toEqual(expect.any(String));
 });
 
 // Test for getSchool()
 test('get school name', ()=> {
-    const intern = new Intern('School');
+    const intern = new Intern('Rob', 23, 'rob@test.com','School');
 
     expect(intern.getSchool().toEqual(expect.any(String)));
 });
 
 // Test getRole()
 test('get intern role', () => {
-    const intern = new Intern('School');
+    const intern = new Intern('Rob', 23, 'rob@test.com','School');
 
     expect(intern.getRole().toBe('Intern'));
 });
