c6a05054ff93dd6d4d5082f89e4172903d26094f
Robert Pliszka <robplisz@gmail.com>
2022-05-07 10:24:34 -0400

    Added failing tests to Engineer

--- __tests__/Manager.test.js
+++ __tests__/Manager.test.js
@@ -1,11 +1,13 @@
 const Manager = require('../lib/Manager.js');
 
+// Tests for manager office number
 test('get employee office number', ()=> {
     const manager = new Manager('Rob', 23, 'rob@email.com');
 
     expect(manager.officeNumber).toEqual(expect.any(Number));
 });
 
+// Tests for Manager role to be displayed
 test('get employee manager role', () => {
     const manager = new Manager('Rob', 23, 'rob@email.com');
 
