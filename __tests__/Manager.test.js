c474a01192a29876ea4fd12d09b892643235ed87
Robert Pliszka <robplisz@gmail.com>
2022-05-07 22:24:08 -0400

    Added Manager class to pass Manager test

--- __tests__/Manager.test.js
+++ __tests__/Manager.test.js
@@ -2,14 +2,14 @@ const Manager = require('../lib/Manager.js');
 
 // Tests for manager office number
 test('get employee office number', ()=> {
-    const manager = new Manager('Rob', 23, 'rob@email.com');
+    const manager = new Manager('Rob', 23, 'rob@email.com', 12);
 
     expect(manager.officeNumber).toEqual(expect.any(Number));
 });
 
 // Tests for Manager role to be displayed
 test('get employee manager role', () => {
-    const manager = new Manager('Rob', 23, 'rob@email.com');
+    const manager = new Manager('Rob', 23, 'rob@email.com', 12);
 
     expect(manager.getRole()).toBe('Manager');
 });
